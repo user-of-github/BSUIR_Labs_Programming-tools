fbb3b742c50293af31d662bebf983801c10b389a
Nikita <nikitosminsk100@gmail.com>
2021-04-01 22:53:00 +0300

    [LR4]: Feat (2nd task - done: DLL C#)

---
+++ LR4 2021.04.01/LibraryDLL/TestDLL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestDLL
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine(Library.Library.CheckIfNumber("2021hello")); // False
+			Console.WriteLine(Library.Library.CheckIfNumber("123456")); // True
+			Console.WriteLine(Library.Library.CheckIfNumber("abcd")); // False
+
+			Console.WriteLine(Library.Library.ConvertToInteger("123456789")); // 123456789
+			Console.WriteLine(Library.Library.ConvertToInteger("98765")); // 98765
+
+			Console.WriteLine(Library.Library.CheckIfPalindrom("1234554321")); // True
+			Console.WriteLine(Library.Library.CheckIfPalindrom("123455432")); // False
+		}
+	}
+}
