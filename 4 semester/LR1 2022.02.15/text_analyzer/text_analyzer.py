b297f87090b97a57ae00fc04205b98354525a7d3
Admin <nikitosminsk100@gmail.com>
2022-02-16 19:47:24 +0300

    Deleted project folder from main branch to copy it then to LR1 branch

--- 4 semester/LR1 2022.02.15/text_analyzer/text_analyzer.py
+++
@@ -1,4 +0,0 @@
-def analyze_text(text: str) -> (int, int):
-    print(text)
-
-    return 4, 5
--- 4 semester/LR1 2022.02.15/utils/utils.py
+++
@@ -1,6 +0,0 @@
-def read_text_from_file(file_name: str) -> str:
-    file = open(file_name, 'r')
-    response = ''
-    for line in file:
-        response += line
-    return response
