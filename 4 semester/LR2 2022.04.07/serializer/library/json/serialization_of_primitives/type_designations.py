d7acd710519be3fe0232bcf968ab03f8c8bd521a
Nikita <nikitosminsk100@gmail.com>
2022-04-11 22:09:40 +0300

    basic serialization for list & dicts with primitives (including dicts and lists)

--- 4 semester/LR2 2022.04.07/serializer/library/json/serialization_of_primitives/type_designations.py
+++
@@ -1,4 +0,0 @@
-NUMBER: str = 'number'
-STRING: str = 'string'
-BOOLEAN: str = 'boolean'
-ARRAY: str = 'array'
--- 4 semester/LR2 2022.04.07/serializer/main.py
+++ 4 semester/LR2 2022.04.07/serializer/main.py
@@ -1,8 +1,14 @@
-from library.json.serialization_of_primitives.auto_serialize_primitive import auto_serialize_primitive
+from library.json.serialization_of_primitives.serialize_primitives import auto_serialize
 
 
 def main() -> None:
-    print(auto_serialize_primitive(False))
+    test_dict: dict = dict()
+    test_dict['kek'] = 'shrek'
+    test_dict['kek2'] = 'shrek2'
+    test_dict['kek3'] = [4, 5, 6, "555"]
+    test_dict[6] = [4, 5, 6, "555"]
+
+    print(auto_serialize(test_dict))
 
 
 if __name__ == '__main__':
